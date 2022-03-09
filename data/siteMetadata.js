ae7a2995e1f126bd3fd9e2632f8ee878788a85cd
Satvik Gaikwad <satvik.g.code@gmail.com>
2022-03-09 11:48:53 +0530

    Fix: Typo

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -20,7 +20,7 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    googleAnalyticsId: process.env.GOOGLE_ANALYTICTS, // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: process.env.GOOGLE_ANALYTICS, // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue
