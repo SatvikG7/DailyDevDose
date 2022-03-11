1800ab35e95e7685c1e4810f9727db43df3c5683
Satvik Gaikwad <satvik.g.code@gmail.com>
2022-03-11 11:50:33 +0530

    Fix: :bug: tag undefined

--- components/analytics/index.js
+++ components/analytics/index.js
@@ -1,7 +1,6 @@
 import GA from "./GoogleAnalytics";
-import siteMetadata from "@/data/siteMetadata";
 const Analytics = () => {
-  return <GA id={siteMetadata.analytics.googleAnalyticsId} />;
+  return <GA />;
 };
 
 export default Analytics;
